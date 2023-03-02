03485b40f4d65857a5cd446daeff1d1ba1453d55
Kushal Devkota <kushal@terpmail.umd.edu>
2023-03-02 16:40:29 -0500

    inital setup

---
+++ TerpExchange/TerpExchange/TerpExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerpExchangeApp.swift
+//  TerpExchange
+//
+//  Created by kushal on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct TerpExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
